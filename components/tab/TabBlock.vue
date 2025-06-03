96c704484300da90a9026e73ab298ce755a1b3e8
Gerhard Seidel <gs@weareindeed.com>
2025-06-03 09:39:55 +0800

    chore: yarn workspace (#2269)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | no
    | License      | MIT
    
    Use yarn workspaces instead of `assets/node_modules` folder. The `node_modules` folder has some disadvantages, as the name is often use to ignore processes inside this folder e.g. watching files, code analysis in ci tools etc. Yarn workspaces is made for mono repositories, so we decide with this solution, because yarn is already used as our package manager.

---
+++ components/tab/TabBlock.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="tab-container">
+
+        <div class="poly-collection-actions" v-if="tab.actions">
+            <template v-for="action in tab.actions">
+                <component :is="action.component" :data="action"></component>
+            </template>
+        </div>
+
+        <form-widget :form="form.get(tab.property)" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import {BlockTab} from "@enhavo/block/tab/BlockTab";
+import {Form} from "@enhavo/vue-form/model/Form";
+import {onMounted} from "vue";
+
+const props = defineProps<{
+    tab: BlockTab,
+    form: Form,
+}>()
+
+onMounted(() => {
+    props.tab.init();
+})
+
+</script>
